58fec48b70b12eab6698c0fb98bbd877bb1e6005
kthordarson@gmail.com <kthordarson@gmail.com>
2023-12-19 13:57:00 +0000

    urlscanio InsecureRequestWarning

--- modules/urlscanio.py
+++ modules/urlscanio.py
@@ -4,7 +4,8 @@ import sys
 from loguru import logger
 import requests
 
-
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_urlscanio_data(ipaddr):
 	# https://urlscan.io/docs/api/
@@ -19,6 +20,13 @@ def search_urlscanio(remoteurl):
 		'q': remoteurl,
 			}
 	url = f'https://urlscan.io/api/v1/search/'
-	response = requests.get(url, params=params, headers=headers, timeout=20)
+	try:
+		response = requests.get(url, params=params, headers=headers, timeout=20, verify=False)
+	except requests.exceptions.Timeout as e:
+		logger.error(f"{e} for {url} {remoteurl}")
+		return None
+	except requests.exceptions.SSLError as e:
+		logger.error(f"{e} {url} {remoteurl}")
+		return None
 	all_json = response.json()
 	return all_json
